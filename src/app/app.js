3a7cb11fc4d6df6c519206fa2c7db1d61099bffb
ushakumarik <k.ushakumari@yahoo.com>
2018-04-05 11:31:36 +0530

    Adding initial files

---
+++ src/app/app.js
@@ -0,0 +1,15 @@
+console.log("hi");
+
+// import React from "react";
+// import ReactDOM from "react-dom";
+
+// var SimpleComponent = React.createClass({
+//   render: function() {
+//     return <div>{this.props.message}</div>;
+//   }
+// });
+
+// ReactDOM.render(
+//   <SimpleComponent message="React Demo" />,
+//   document.querySelector(".js-app")
+// );
